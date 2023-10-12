f862ae94a09fc4c3002bbdcd99b32414032bc2d6
hamzakhouzima <khouzimahamza20@gmail.com>
2023-10-12 16:58:37 +0100

    setup and database creation with tables

---
+++ src/main/java/com/optima/demo12/Employee.java
@@ -0,0 +1,30 @@
+package com.optima.demo12;
+
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "employee")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "username")
+    private String username;
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "password")
+    private String password;
+    @Column(name = "date")
+    private String date;
+    // Getters and setters
+    // ...
+}
